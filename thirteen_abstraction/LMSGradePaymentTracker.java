4be66891b4af195349076c929b1a1a7982d1f765
Anusha498 <ralla.chiru@gmail.com>
2025-08-28 11:39:23 +0530

    LMS with Abstraction - P2

---
+++ thirteen_abstraction/LMSGradePaymentTracker.java
@@ -0,0 +1,21 @@
+package thirteen_abstraction;
+
+public class LMSGradePaymentTracker {
+    public static void main(String[] args) {
+
+        // Now implementing Abstraction -> Student
+        Personable student = new Student();
+        student.setPersonDetails();;
+        student.displayPersonDetails();
+
+        ((Payables)student).calculatePayment();
+        student.achievementStatus();
+
+        // Now implementing Abstraction -> Trainer
+        Personable trainer = new Trainer();
+        trainer.setPersonDetails();
+        trainer.displayPersonDetails();
+        ((Payables)trainer).calculatePayment();
+
+    }
+}
